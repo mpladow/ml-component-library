45628f288d561e933bc54b08ed17206906832092
Michael La Dow <mpladow@gmail.com>
2024-08-22 23:04:33 +1000

    add custom media query hook

--- src/Components/Container/Container.tsx
+++ src/Components/Container/Container.tsx
@@ -1,10 +1,16 @@
+import { useMediaQuery } from '@uidotdev/usehooks';
+import { useEffect } from 'react';
 import { ScrollView, StyleSheet, Text, View, type StyleProp, type ViewProps } from 'react-native'
+import { useCustomMediaQuery } from '../../hooks';
 
 type ContainerType = {
 	isScrollable?: boolean;
 	contentContainerStyle?: StyleProp<ViewProps>
 } & ViewProps
 const Container = ({ isScrollable = true, contentContainerStyle, children, style, }: ContainerType) => {
+	const size = useCustomMediaQuery();
+	useEffect(() => {
+	}, [size])
 	return (
 		isScrollable ? <ScrollView contentContainerStyle={contentContainerStyle}
 			keyboardShouldPersistTaps="always"
